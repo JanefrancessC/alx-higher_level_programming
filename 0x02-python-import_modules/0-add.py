9c4af0cb2d7c14b2c1e0b04403e2a82135002d92
JanefrancessC <janefrancesschioma@gmail.com>
2023-11-02 11:09:53 -0700

    0-add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add
-a = 1
-b = 2
-result = add(a, b)
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    result = add(a, b)
 print('{2} + {1} = {0}'.format(result, b, a))
