3f8370b6a9909cbbc82cf6a1f634b4db0413f4bd
JanefrancessC <janefrancesschioma@gmail.com>
2023-11-08 19:01:06 -0800

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
-
+def best_score(a_dictionary):
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+        return None
+    best_val = list(a_dictionary.keys())[0]
+    big_val = a_dictionary[best_val]
+    for k, v in a_dictionary.items():
+        if v > big_val:
+            big_val = v
+            best_val = k
+    return (best_val)
