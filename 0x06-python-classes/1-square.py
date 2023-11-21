32cf5f3066b1a13446453147017cf17f5454751d
JanefrancessC <janefrancesschioma@gmail.com>
2023-11-21 15:30:17 -0800

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
+class Square:
+    def __init__(self, size):
+        """size is a private attribute for class Square"""
+        self.__size = size
