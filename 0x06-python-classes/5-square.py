fff4983ec25178c570b14cd6612f27ec5c88393c
JanefrancessC <janefrancesschioma@gmail.com>
2023-11-21 16:42:58 -0800

    5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
+class Square:
+    """Square """
+    def __init__(self, size=0):
+        """size is a private attribute for class Square
+        Args:
+            size (int) with a default value of 0
+        Raises:
+            TypeError if size is not an integer
+            ValueError if size < 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Returns an int - area of a square
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Retrieves size """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """
+        prints in stdout the square with the character #
+        """
+        if self.size == 0:
+            print()
+        else:
+            for i in range(self.size):
+                for j in range(self.size):
+                    print("{}".format("#"), end="")
+                print()
