d0a30be7f87079cebadf83c31024a8d9ade8cc01
JanefrancessC <janefrancesschioma@gmail.com>
2023-11-27 13:41:00 -0800

    1-rectangle.py

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Define a Rectangle class"""
+
+
+class Rectangle:
+    """ class Rectangle with private instance attributes """
+    def __init__(self, width=0, height=0):
+        """ width and height are private attributes of Rectangle
+        Args:
+        width (int) - default value is 0
+        height (int) - default value = 0
+
+        Raises:
+        TypeError - width/height  must be an integer
+        ValueError - width/height must be >= 0 
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
