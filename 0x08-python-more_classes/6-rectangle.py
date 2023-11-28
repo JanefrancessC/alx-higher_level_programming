f6f5726c29441c121cc729dc06d929f8ca033e18
JanefrancessC <janefrancesschioma@gmail.com>
2023-11-28 06:13:44 -0800

    6-rectangle style fix

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -5,6 +5,7 @@
 class Rectangle:
     """A Rectangle class"""
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """width and height are private attributes for class Rectangle
         Args:
