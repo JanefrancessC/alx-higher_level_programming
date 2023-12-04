a6cb9ae0de3b1c5c0a94231bf8b623d6a81de25c
JanefrancessC <janefrancesschioma@gmail.com>
2023-12-04 14:03:05 -0800

    8-rectangle.py

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Define a BaseGeometry class"""
+
+
+class BaseGeometry:
+    """raises an exception"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        validates input value
+        Args:
+        name (string) - user input
+        value (int) - value to validate
+        Raises:
+            TypeError: if value not an integer
+            ValueError: if value <= 0
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """inherits from BaseGeometry class"""
+
+    def __init__(self, width, height):
+        self.__width = width
+        self.height = height
+        BaseGeometry().integer_validator("width", width)
+        BaseGeometry().integer_validator("height", height)
