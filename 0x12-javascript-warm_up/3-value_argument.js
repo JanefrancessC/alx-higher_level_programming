7f6392af2b1ccba842e1e798bfd1b01facd4b9d2
JanefrancessC <janefrancesschioma@gmail.com>
2024-02-15 06:41:09 -0800

    3-value_argument.js re

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-const [argc] = process.argv.slice(2)
-if (argc === undefined) console.log("No argument")
-else console.log(argc)
+const [argc] = process.argv.slice(2);
+if (argc === undefined) console.log("No argument");
+else console.log(argc);
