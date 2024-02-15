8f0ed8e4149d9f53323fe549741f1b4db454302e
JanefrancessC <janefrancesschioma@gmail.com>
2024-02-15 06:44:15 -0800

    fixing style

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
-const numArgs = process.argv.length - 2
+const numArgs = process.argv.length - 2;
 
-if (!numArgs) {
-	console.log("No argument")
-} else if (numArgs === 1) {
-	console.log("Argument found")
-} else {
-	console.log("Arguments found")
-}
+if (!numArgs) console.log('No argument');
+else if (numArgs === 1) console.log('Argument found');
+else console.log('Arguments found');
+
